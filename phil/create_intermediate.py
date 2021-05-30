0272359ac2946afcbe5c5f5b43107e39220868b9
Philip Mateo Pfeffer <philipmateopfeffer@DNa1ca0e9.SUNet>
2021-05-30 15:47:09 -0700

    added arg parsing, changed statistic collection, changed modal amodal generation and added checks

--- phil/create_intermediate.py
+++ phil/create_intermediate.py
@@ -97,62 +97,57 @@ intermediate_dir
                 amodal
    
 """
-
-# Mix data sets
 import os
 from util import *
 import shutil
-import glob
-import random
 from datetime import datetime
 import numpy as np
+import argparse
+from pathlib import Path
 from create_modal_boxes import *
-
-AUG_DIR = os.path.join(os.path.dirname(__file__), './augmented20/') # Augmented data
-ORIG_DIR = os.path.join(os.path.dirname(__file__), './first20_pre_intermediate/') # Original data
-INTERMEDIATE_DIR = os.path.join(os.path.dirname(__file__), './intermediate_data/')
-
-
 
 
 def make_orig_dirs(intermediate_dir):
     for split in ['train', 'val', 'test']:
-        new_dir = INTERMEDIATE_DIR + '/images/' + split + '/orig/'
+        new_dir = intermediate_dir + '/images/' + split + '/orig/'
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
-        new_dir = INTERMEDIATE_DIR + '/images/' + split + '/orig/'
+        new_dir = intermediate_dir + '/images/' + split + '/orig/'
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
 
     for split in ['train', 'val', 'test']:
-        new_dir = INTERMEDIATE_DIR + '/labels/' + split + '/orig/amodal/'
+        new_dir = intermediate_dir + '/labels/' + split + '/orig/amodal/'
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
-        new_dir = INTERMEDIATE_DIR + '/labels/' + split + '/orig/modal/'
+        new_dir = intermediate_dir + '/labels/' + split + '/orig/modal/'
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
 
 def make_aug_dirs(intermediate_dir):
     for split in ['train', 'val', 'test']:
-        new_dir = INTERMEDIATE_DIR + '/images/' + split + '/aug/'
+        new_dir = intermediate_dir + '/images/' + split + '/aug/'
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
-        new_dir = INTERMEDIATE_DIR + '/images/' + split + '/aug/'
+        new_dir = intermediate_dir + '/images/' + split + '/aug/'
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
 
     for split in ['train', 'val', 'test']:
-        new_dir = INTERMEDIATE_DIR + '/labels/' + split + '/aug/'
+        new_dir = intermediate_dir + '/labels/' + split + '/aug/'
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
-        new_dir = INTERMEDIATE_DIR + '/labels/' + split + '/aug/'
+        new_dir = intermediate_dir + '/labels/' + split + '/aug/'
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
 
+def check_modal_amodal(image_name, modal_yolo_label, amodal_yolo_label):
+    assert amodal_yolo_label.shape[0] == modal_yolo_label.shape[0], f"Modal and Amodal labels have different number of annotations for image {image_name}"
+    if modal_yolo_label.size != 0:
+        assert ~np.any(modal_yolo_label[:,1:5] < 0), f"Modal box has negative value for image {image_name}."
 
 
-
-def copy_orig(orig_data_dir=ORIG_DIR, intermediate_dir=INTERMEDIATE_DIR):
+def copy_orig(orig_data_dir, intermediate_dir):
     make_orig_dirs(intermediate_dir)
 
     orig_images_path = orig_data_dir + "images/"
@@ -163,9 +158,9 @@ def copy_orig(orig_data_dir=ORIG_DIR, intermediate_dir=INTERMEDIATE_DIR):
     if '.DS_Store' in orig_labels_yolo:
         orig_labels_yolo.remove('.DS_Store')
     
-    print("Starting original images")
+    print(f"[{datetime.now()}] Starting original images")
     img_count = 0
-    modal_amodal_different_counts = {}  # { "train": Int, "val": Int, "test": Int }
+    modal_amodal_different_counts = {}  # { "train": List[int, int], "val": List[int, int], "test": List[int, int] }
     for split in os.listdir(orig_images_path):
         print(f"[{datetime.now()}] Starting {split}")
         
@@ -173,10 +168,10 @@ def copy_orig(orig_data_dir=ORIG_DIR, intermediate_dir=INTERMEDIATE_DIR):
             continue
         
         orig_images_split_path = orig_images_path + split + '/'
-        orig_labels_yolo_split_path = orig_labels_path_yolo + split + '/'
+        # orig_labels_yolo_split_path = orig_labels_path_yolo + split + '/'
         orig_labels_kitti_split_path = orig_labels_path_kitti + split + '/'
         
-        modal_amodal_different_count = 0
+        modal_amodal_different_count = [0,0]
         for image_filename in os.listdir(orig_images_split_path):
             if image_filename.find(".DS_Store") != -1:
                 continue
@@ -186,40 +181,39 @@ def copy_orig(orig_data_dir=ORIG_DIR, intermediate_dir=INTERMEDIATE_DIR):
             d_img = os.path.join(intermediate_dir, "images/" + split + "/orig/")
             shutil.copy2(s_img, d_img)
 
-            # Copy YOLO label
+            # Create and write amodal YOLO label
             image_name, _ = get_filename_and_extension(image_filename)
-            s_label = os.path.join(os.path.dirname(__file__), orig_labels_yolo_split_path + image_name + '.txt')
-            d_label = os.path.join(intermediate_dir, "labels/" + split + "/orig/amodal/")
-            shutil.copy2(s_label, d_label)
-            
-            # Create and write amodal KITTI label
             amodal_kitti_label_path = os.path.join(os.path.dirname(__file__), orig_labels_kitti_split_path + image_name + '.txt')
             amodal_kitti_label = read_background_annotation_kitti(amodal_kitti_label_path)
-            modal_kitti_label, modal_amodal_different_count = generate_modal_label(amodal_kitti_label, modal_amodal_different_count)
-            modal_yolo_label = kitti_to_yolo(modal_kitti_label)
-            d_label_modal_filepath = os.path.join(intermediate_dir, "labels/" + split + "/orig/modal/" + image_name + '.txt')
-            write_modal_label_to_file(modal_yolo_label, d_label_modal_filepath)
+            modal_kitti_label, amodal_kitti_label, modal_amodal_different_count = generate_modal_label(amodal_kitti_label, modal_amodal_different_count)
+            modal_yolo_label = kitti_label_to_yolo_label(modal_kitti_label)
+            amodal_yolo_label = kitti_label_to_yolo_label(amodal_kitti_label)
+            modal_label_filepath = os.path.join(intermediate_dir, "labels/" + split + "/orig/modal/" + image_name + '.txt')
+            amodal_label_filepath = os.path.join(intermediate_dir, "labels/" + split + "/orig/amodal/" + image_name + '.txt')
+            write_label_to_file(modal_yolo_label, modal_label_filepath)
+            write_label_to_file(amodal_yolo_label, amodal_label_filepath)
 
+            check_modal_amodal(image_name, modal_yolo_label, amodal_yolo_label)
             img_count += 1
             if img_count % 100 == 0:
-                print(f"Finished {img_count} images")
+                print(f"[{datetime.now()}] Finished {img_count} images")
             
         modal_amodal_different_counts[split] = modal_amodal_different_count
         
-    print("Finishes original images")
-    print("Original modal-amodal-different Count: ", modal_amodal_different_counts)
+    print(f"[{datetime.now()}] Finished original images")
+    print(f"[{datetime.now()}] Original modal-amodal-different Count: ", modal_amodal_different_counts)
 
 
 
 
-def copy_aug(aug_data_dir_path=AUG_DIR, intermediate_dir=INTERMEDIATE_DIR, orig_data_dir=ORIG_DIR):
+def copy_aug(aug_data_dir_path, intermediate_dir, orig_data_dir):
     splits = os.listdir(aug_data_dir_path)
     if '.DS_Store' in splits:
         splits.remove('.DS_Store')
 
     img_count = 0
-    print("Starting to copy augmented images")
-    modal_amodal_different_counts = {}  # { "train": Int, "val": Int, "test": Int }
+    print(f"[{datetime.now()}] Starting to copy augmented images")
+    modal_amodal_different_counts = {}  # { "train": List[int, int], "val": List[int, int], "test": List[int, int] }
 
     for split in splits:
         print(f"[{datetime.now()}] Starting {split}")
@@ -228,7 +222,7 @@ def copy_aug(aug_data_dir_path=AUG_DIR, intermediate_dir=INTERMEDIATE_DIR, orig_
         if '.DS_Store' in occlusion_classes:
             occlusion_classes.remove('.DS_Store')
 
-        modal_amodal_different_count = 0
+        modal_amodal_different_count = [0,0]
         for occlusion_class in occlusion_classes:
             occlusion_class_path = split_path + occlusion_class + '/'
             images_dir_path = occlusion_class_path + 'images/'
@@ -253,7 +247,7 @@ def copy_aug(aug_data_dir_path=AUG_DIR, intermediate_dir=INTERMEDIATE_DIR, orig_
                 occlusion_yolo_label_path = os.path.join(labels_dir_path, image_id + '.annotated.' + occlusion_id + '.txt')
 
                 # Get occlusion and original kitti labels
-                original_yolo_label_filepath = orig_data_dir + 'yolo_labels/' + split + '/' + image_id + '.txt'
+                # original_yolo_label_filepath = orig_data_dir + 'yolo_labels/' + split + '/' + image_id + '.txt'
                 original_kitti_label_filepath = orig_data_dir + 'kitti_labels/' + split + '/' + image_id + '.txt'
                 original_amodal_kitti_label = read_background_annotation_kitti(original_kitti_label_filepath)
 
@@ -265,44 +259,65 @@ def copy_aug(aug_data_dir_path=AUG_DIR, intermediate_dir=INTERMEDIATE_DIR, orig_
                 concat_amodal_kitti_label = np.concatenate((original_amodal_kitti_label, occlusion_amodal_kitti_label))
 
                 # Get modal bboxes
-                concat_modal_kitti_label, modal_amodal_different_count = generate_modal_label(concat_amodal_kitti_label, modal_amodal_different_count)
+                concat_modal_kitti_label, concat_amodal_kitti_label, modal_amodal_different_count = generate_modal_label(concat_amodal_kitti_label, modal_amodal_different_count)
                 modal_kitti_label = concat_modal_kitti_label[:-num_occlusions,:]    # Discard occluder labels
-                modal_yolo_label = kitti_to_yolo(modal_kitti_label)
+                amodal_kitti_label = concat_amodal_kitti_label[:-num_occlusions,:]    # Discard occluder labels
+                modal_yolo_label = kitti_label_to_yolo_label(modal_kitti_label)
+                amodal_yolo_label = kitti_label_to_yolo_label(amodal_kitti_label)
 
                 # Write amodal YOLO label
-                d_label_dir = os.path.join(intermediate_dir, 'labels/' + split + '/aug/' + occlusion_class + '/amodal/')
-                if not os.path.exists(d_label_dir):
-                    os.makedirs(d_label_dir)
-                d_label = os.path.join(d_label_dir, image_id + '.annotated.' + occlusion_id + '.txt')
-                shutil.copy2(original_yolo_label_filepath, d_label)
+                d_label_amodal_dir = os.path.join(intermediate_dir, 'labels/' + split + '/aug/' + occlusion_class + '/amodal/')
+                if not os.path.exists(d_label_amodal_dir):
+                    os.makedirs(d_label_amodal_dir)
+                d_label_amodal_filepath = os.path.join(d_label_amodal_dir, image_id + '.annotated.' + occlusion_id + '.txt')
+                write_label_to_file(amodal_yolo_label, d_label_amodal_filepath)
 
                 # Write modal YOLO label
                 d_label_modal_dir = os.path.join(intermediate_dir, "labels/" + split + '/aug/' + occlusion_class + '/modal/')
                 if not os.path.exists(d_label_modal_dir):
                     os.makedirs(d_label_modal_dir)
                 d_label_modal_filepath = os.path.join(d_label_modal_dir, image_id + '.annotated.' + occlusion_id + '.txt')
-                write_modal_label_to_file(modal_yolo_label, d_label_modal_filepath)
+                write_label_to_file(modal_yolo_label, d_label_modal_filepath)
 
+                check_modal_amodal(image_name, modal_yolo_label, amodal_yolo_label)
                 img_count += 1
                 if img_count % 100 == 0:
-                    print(f"Finished {img_count} images")
+                    print(f"[{datetime.now()}] Finished {img_count} images")
 
         modal_amodal_different_counts[split] = modal_amodal_different_count
     
-    print("Augmented modal-amodal-different Count: ", modal_amodal_different_counts)
-    print("Finished augmented images")
+    print(f"[{datetime.now()}] Augmented modal-amodal-different Count: ", modal_amodal_different_counts)
+    print(f"[{datetime.now()}] Finished augmented images")
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--orig-root', type=str, help="top-level dir where original data is stored")
+    parser.add_argument('--aug-root', type=str, help="top-level dir where augmented data is stored")
+    parser.add_argument('--inter-root', type=str, help="top-level dir where intermediate data should be stored")
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
+    # Get args
+    args = get_args()
+    orig_path = os.path.join(os.path.dirname(__file__), args.orig_root + '/')
+    aug_path = os.path.join(os.path.dirname(__file__), args.aug_root + '/')
+    inter_path = os.path.join(os.path.dirname(__file__), args.inter_root + '/')
+
     # Create output dirs
-    if not os.path.exists(INTERMEDIATE_DIR):
-        os.makedirs(INTERMEDIATE_DIR)
+    if not os.path.exists(inter_path):
+        os.makedirs(inter_path)
     
     # # Copy original images into output dir
-    copy_orig(ORIG_DIR, INTERMEDIATE_DIR)
+    copy_orig(orig_path, inter_path)
 
     # Copy augmented images into output dir
-    make_aug_dirs(INTERMEDIATE_DIR)     # Because we aren't using the aug for the baseline, but future scripts expect this structure
-    # copy_aug(AUG_DIR, INTERMEDIATE_DIR, ORIG_DIR)
+    make_aug_dirs(inter_path)     # Because we aren't using the aug for the baseline, but future scripts expect this structure
+    # copy_aug(aug_path, inter_path, orig_path)
+
+"""
+Example Usage:
+python create_intermediate.py --orig-root first20_pre_intermediate --aug-root augmented20 --inter-root intermediate_data
+python create_intermediate.py --orig-root data --aug-root augmented20 --inter-root intermediate_data
+"""
