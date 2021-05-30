e18e3ae7fc0f9d2a047dfaa646bf3cc7c7628959
Ashish Jain <ashishjain87@gmail.com>
2021-05-29 22:37:31 -0700

    Added Danny's code

--- combined/collapse-labels/CollapseAllLabels.py
+++ combined/collapse-labels/CollapseAllLabels.py
@@ -3,7 +3,11 @@ import logging.config
 import os
 import glob
 import yaml
+import numpy as np
 from tqdm import tqdm
+
+KITTI_CATEGORY_TO_COLLAPSED_CATEGORY = {3: 1, 4: 1, 5: 1, 0: 0, 1: 0, 2: 0, 6: 0, 7: 2, 8: 2}
+CLASS_NAMES_MAP = {0: "vehicle", 1: "person", 2: "misc"}
 
 def setup_logging(logging_config_path: str = 'logging.yaml', default_level: int = logging.INFO) -> None:
     if os.path.exists(logging_config_path):
@@ -15,6 +19,24 @@ def setup_logging(logging_config_path: str = 'logging.yaml', default_level: int 
 
 def get_immediate_subdirectories(super_dir):
     return [name for name in os.listdir(super_dir) if os.path.isdir(os.path.join(super_dir, name))]
+
+def read_yolo(path_to_label_file: str) -> np.ndarray:
+    yolo_label = np.genfromtxt(path_to_label_file, delimiter=" ", dtype=float, encoding=None)
+    return clean_yolo(yolo_label)
+
+def clean_yolo(yolo_label: np.ndarray):
+    if len(yolo_label.shape) == 1:  # if there's only one label, shape will be (5,) which is 1-d, this fixes it.
+        yolo_label = np.array([yolo_label])
+    return yolo_label
+
+def write_label_to_file(labels: np.ndarray, filepath: str):
+    np.savetxt(filepath, labels, delimiter=" ")
+
+def collapse_labels(path_to_label_file_in: str, path_to_label_file_out: str):
+    yolo_label = read_yolo(path_to_label_file_in)
+    for i in range(yolo_label.shape[0]):
+        yolo_label[i, 0] = KITTI_CATEGORY_TO_COLLAPSED_CATEGORY[int(yolo_label[i, 0])]
+    write_label_to_file(yolo_label, path_to_label_file_out)
 
 def main():
     # Logging Setup
@@ -68,7 +90,9 @@ def main():
     logger.info("Number of labels found: %i", len(label_input_ouput_paths_dict))
     for input_file_path in tqdm(label_input_ouput_paths_dict, desc="Process files"):
         output_file_path = label_input_ouput_paths_dict[input_file_path]
-        logger.debug("Input: %s, Output: %s", input_file_path, output_file_path)
+        collapse_labels(input_file_path, output_file_path)
+        logger.debug("Processed Input : %s", input_file_path)
+        logger.debug("Generated Output: %s", output_file_path)
 
     logger.info('Finished')
 
