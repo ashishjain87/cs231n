14c31e4ebfd2318f5bda4876edc888870678a337
Philip Mateo Pfeffer <philipmateopfeffer@DNa1ca0e9.SUNet>
2021-05-30 15:48:46 -0700

    Now also modify amodal label if objects are fully occluded

--- phil/create_modal_boxes.py
+++ phil/create_modal_boxes.py
@@ -9,33 +9,39 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.patches as patches
 import copy
-from typing import Tuple
+from typing import Tuple, List
 
-def plot_box(ax, entry, color='r',):
-    width = entry[6]-entry[4]
-    height = entry[7]-entry[5]
-    rect = patches.Rectangle((entry[4], entry[5]), width, height, linewidth=1, edgecolor=color, facecolor='none')
+def plot_kitti_box(ax, kitti_entry, color='r'):
+    width = kitti_entry[6]-kitti_entry[4]
+    height = kitti_entry[7]-kitti_entry[5]
+    rect = patches.Rectangle((kitti_entry[4], kitti_entry[5]), width, height, linewidth=1, edgecolor=color, facecolor='none')
     ax.add_patch(rect)
 
-def show_all_labels(image_path, amodal_labels, modal_labels):
+def show_modal_amodal(image_path: str, amodal_kitti_label: np.ndarray, modal_kitti_label: np.ndarray):
     image = np.array(plt.imread(image_path))
-    fig, ax = plt.subplots()
+    _, ax = plt.subplots()
     ax.imshow(image)
     
     # Draw each bounding box
-    assert len(modal_labels) == len(amodal_labels)
-    for label_idx in range((len(modal_labels))):
-        modal_entry = modal_labels[label_idx]
-        amodal_entry = amodal_labels[label_idx]
+    assert len(modal_kitti_label) == len(amodal_kitti_label)
+    for label_idx in range((len(modal_kitti_label))):
+        modal_entry = modal_kitti_label[label_idx]
+        amodal_entry = amodal_kitti_label[label_idx]
         if list(modal_entry) != list(amodal_entry):
-            plot_box(ax, amodal_entry, 'r')
-        plot_box(ax, modal_entry, 'b')
+            plot_kitti_box(ax, amodal_entry, 'r')
+        plot_kitti_box(ax, modal_entry, 'b')
     
     plt.show()
     
 def filter_full_overlap(modal_labels: np.ndarray) -> np.ndarray:
     modal_labels = modal_labels[modal_labels[:,4] != -1]
     return modal_labels
+
+def larger_object_is_occluder(larger_object_occlusion_state: float) -> bool:
+    return int(larger_object_occlusion_state) == OCCLUDER_OCCLUSION_STATE
+
+def larger_object_less_occluded(label1_occlusion: float, label2_occlusion: float) -> bool:
+    return label2_occlusion < label1_occlusion
 
 def label2_infront(label1, label2):
     obj1_class, _, obj1_occlusion, _, left1, top1, right1, bottom1 = label1[:8]
@@ -59,7 +65,7 @@ def label2_infront(label1, label2):
     obj2_taller_similar_class = similar_class and ((bottom2-top2) > (bottom1-top1))
     return obj1_more_occluded or obj2_taller_similar_class
     
-def generate_modal_label(amodal_kitti_label: np.ndarray, modal_amodal_different_count: int) -> Tuple[np.ndarray, int]:
+def generate_modal_label(amodal_kitti_label: np.ndarray, modal_amodal_different_count: List[int]) -> Tuple[np.ndarray, np.ndarray, int]:
     modal_labels = copy.deepcopy(amodal_kitti_label)
     num_labels = len(modal_labels)
 
@@ -77,38 +83,46 @@ def generate_modal_label(amodal_kitti_label: np.ndarray, modal_amodal_different_
                 
                 # We assume that label 2 is the larger object
                 modal_label1_coords = copy.deepcopy(label1_coords)
-                if (left2 <= left1) and (right2 >= right1) and (top2 <= top1) and (bottom2 >= bottom1) and label2_occlusion < 0 and label1_occlusion >= 0:
-                    # Occlusion fully covers non-occlusion object (if another object fully covers, we cannot be sure which is in front)
-                    modal_label1_coords = [-1,-1,-1,-1]
+                if (left2 <= left1) and (right2 >= right1) and (top2 <= top1) and (bottom2 >= bottom1):
+                    if larger_object_is_occluder(label2_occlusion) or larger_object_less_occluded(label1_occlusion, label2_occlusion):
+                        # Occlusion fully covers non-occlusion object (if another object fully covers, we cannot be sure which is in front)
+                        modal_label1_coords = [-1,-1,-1,-1]
+                        amodal_kitti_label[label1_idx, 4:8] = [-1,-1,-1,-1]
 
                 elif ((left2 <= left1) and (right2 >= right1)):
                     # Amodal -> Modal horizontal
                     if label2_infront(label1, label2):
-                        new_top = bottom2 if (top1 < bottom2 and top1 > top2) else top1
-                        new_bottom = top2 if (bottom1 < bottom2 and bottom1 > top2) else bottom1
-                        modal_label1_coords[1] = new_top
-                        modal_label1_coords[3] = new_bottom
+                        new_top = bottom2 if (top1 <= bottom2 and top1 >= top2) else top1
+                        new_bottom = top2 if (bottom1 <= bottom2 and bottom1 >= top2) else bottom1
+                        modal_label1_coords[1] = new_top if new_top <= bottom1 else -1
+                        amodal_kitti_label[label1_idx, 5] = amodal_kitti_label[label1_idx, 5] if new_top <= bottom1 else -1
+                        modal_label1_coords[3] = new_bottom if new_bottom >= top1 else -1
+                        amodal_kitti_label[label1_idx, 7] = amodal_kitti_label[label1_idx, 7] if new_bottom >= top1 else -1
+                        assert new_top <= new_bottom, f"Negative height for label {label1_idx+1} due to overlap with label {label2_idx+1}"
 
                 elif ((bottom2 >= bottom1) and (top2 <= top1)):
                     # Amodal -> Modal veritcal
                     if label2_infront(label1, label2):
-                        new_left = right2 if (left1 > left2 and left1 < right2) else left1
-                        new_right = left2 if (right1 > left2 and right1 < right2) else right1
-                        modal_label1_coords[0] = new_left
-                        modal_label1_coords[2] = new_right
+                        new_left = right2 if (left1 >= left2 and left1 <= right2) else left1
+                        new_right = left2 if (right1 >= left2 and right1 <= right2) else right1
+                        modal_label1_coords[0] = new_left if new_left <= right1 else -1
+                        amodal_kitti_label[label1_idx, 4] = amodal_kitti_label[label1_idx, 4] if new_left <= right1 else -1
+                        modal_label1_coords[2] = new_right if new_right >= left1 else -1
+                        amodal_kitti_label[label1_idx, 6] = amodal_kitti_label[label1_idx, 6] if new_right >= left1 else -1
+                        assert new_right >= new_left, f"Negative width for label {label1_idx+1} due to overlap with label {label2_idx+1}"
 
                 if list(modal_label1_coords) != list(label1_coords):
                     # TODO: append all, record how many are different
-                    modal_amodal_different_count += 1
+                    modal_amodal_different_count[0] += 1
 
                 modal_label = list(label1[:4]) + list(modal_label1_coords) + list(label1[8:])
                 modal_labels[label1_idx] = modal_label
     
-    modal_labels = filter_full_overlap(modal_labels)
-    return modal_labels, modal_amodal_different_count
+    modal_amodal_different_count[1] += modal_labels.shape[0]
+    filtered_modal_kitti_labels = filter_full_overlap(modal_labels)
+    filtered_amodal_kitti_labels = filter_full_overlap(amodal_kitti_label)
+    return filtered_modal_kitti_labels, filtered_amodal_kitti_labels, modal_amodal_different_count
 
-def write_modal_label_to_file(modal_label, filepath):
-    np.savetxt(filepath, modal_label, delimiter=" ")
 
 def create_all_modal_labels(images_dir, labels_dir, show_images=False, show_different_modal_amodal=False):
     modal_amodal_different_counts = {}  # { "train": Int, "val": Int, "test": Int }
@@ -117,7 +131,7 @@ def create_all_modal_labels(images_dir, labels_dir, show_images=False, show_diff
         if dataset_split.name == ".DS_Store":
             continue
 
-        modal_amodal_different_count = 0
+        modal_amodal_different_count = [0,0]
         for kitti_label_path in os.scandir(dataset_split):
             if kitti_label_path.name == ".DS_Store":
                 continue
@@ -132,7 +146,7 @@ def create_all_modal_labels(images_dir, labels_dir, show_images=False, show_diff
             
             if show_images:
                 print(kitti_label_path.name)
-                show_all_labels(image_path, amodal_kitti_label, modal_labels)
+                show_modal_amodal(image_path, amodal_kitti_label, modal_labels)
 
         modal_amodal_different_counts[dataset_split.name] = modal_amodal_different_count
     
@@ -162,9 +176,9 @@ def run_create_modal_boxes_tests():
 if __name__ == "__main__":
     # run_create_modal_boxes_tests()
     # labels_dir = os.path.join(os.path.dirname(__file__), "../../cs231_project/data_object_image/training/label_2/")
-    labels_dir = os.path.join(os.path.dirname(__file__), "./first20/kitti_labels/")
-    images_dir = os.path.join(os.path.dirname(__file__), "./first20/images/")
-    # labels_dir = os.path.join(os.path.dirname(__file__), "./data/kitti_labels/")
-    # images_dir = os.path.join(os.path.dirname(__file__), "./data/images/")
-    modal_amodal_different_counts = create_all_modal_labels(images_dir, labels_dir, show_images=True, show_different_modal_amodal=False)
+    # labels_dir = os.path.join(os.path.dirname(__file__), "./first20/kitti_labels/")
+    # images_dir = os.path.join(os.path.dirname(__file__), "./first20/images/")
+    labels_dir = os.path.join(os.path.dirname(__file__), "./data/kitti_labels/")
+    images_dir = os.path.join(os.path.dirname(__file__), "./data/images/")
+    modal_amodal_different_counts = create_all_modal_labels(images_dir, labels_dir, show_images=False, show_different_modal_amodal=False)
     print(modal_amodal_different_counts)
