f955ed4e3601fb7c59754a4f1ce2ee1925fb11b4
Philip Mateo Pfeffer <philipmateopfeffer@DNa1ca0e9.SUNet>
2021-05-31 00:18:05 -0700

    Account for affixers, overall aug copy done, stats corrected, usage and command line args

--- phil/create_intermediate.py
+++ phil/create_intermediate.py
@@ -146,9 +146,14 @@ def check_modal_amodal(image_name, modal_yolo_label, amodal_yolo_label):
     if modal_yolo_label.size != 0:
         assert ~np.any(modal_yolo_label[:,1:5] < 0), f"Modal box has negative value for image {image_name}."
 
+def get_split_type(split: str) -> str:
+    if split.find("train") != -1: return "train"
+    if split.find("val") != -1: return "val"
+    if split.find("test") != -1: return "test"
 
-def copy_orig(orig_data_dir, intermediate_dir):
-    make_orig_dirs(intermediate_dir)
+
+def copy_orig(orig_data_dir, intermediate_dir, aug_data_dir_path):
+    # make_orig_dirs(intermediate_dir)
 
     orig_images_path = orig_data_dir + "images/"
     orig_labels_path_yolo = orig_data_dir + "yolo_labels/"
@@ -161,15 +166,15 @@ def copy_orig(orig_data_dir, intermediate_dir):
     print(f"[{datetime.now()}] Starting original images")
     img_count = 0
     modal_amodal_different_counts = {}  # { "train": List[int, int], "val": List[int, int], "test": List[int, int] }
-    for split in os.listdir(orig_images_path):
-        print(f"[{datetime.now()}] Starting {split}")
-        
+    for split in os.listdir(aug_data_dir_path):
         if split.find(".DS_Store") != -1:
             continue
+        print(f"[{datetime.now()}] Starting {split}")
+        split_type = get_split_type(split)
         
-        orig_images_split_path = orig_images_path + split + '/'
+        orig_images_split_path = orig_images_path + split_type + '/'
         # orig_labels_yolo_split_path = orig_labels_path_yolo + split + '/'
-        orig_labels_kitti_split_path = orig_labels_path_kitti + split + '/'
+        orig_labels_kitti_split_path = orig_labels_path_kitti + split_type + '/'
         
         modal_amodal_different_count = [0,0]
         for image_filename in os.listdir(orig_images_split_path):
@@ -178,8 +183,10 @@ def copy_orig(orig_data_dir, intermediate_dir):
 
             # Copy image
             s_img = os.path.join(os.path.dirname(__file__), orig_images_split_path + image_filename)
-            d_img = os.path.join(intermediate_dir, "images/" + split + "/orig/")
-            shutil.copy2(s_img, d_img)
+            d_dir = os.path.join(intermediate_dir, "images/" + split + "/orig/")
+            if not os.path.exists(d_dir):
+                os.makedirs(d_dir)
+            shutil.copy2(s_img, d_dir)
 
             # Create and write amodal YOLO label
             image_name, _ = get_filename_and_extension(image_filename)
@@ -188,8 +195,16 @@ def copy_orig(orig_data_dir, intermediate_dir):
             modal_kitti_label, amodal_kitti_label, modal_amodal_different_count = generate_modal_label(amodal_kitti_label, modal_amodal_different_count)
             modal_yolo_label = kitti_label_to_yolo_label(modal_kitti_label)
             amodal_yolo_label = kitti_label_to_yolo_label(amodal_kitti_label)
-            modal_label_filepath = os.path.join(intermediate_dir, "labels/" + split + "/orig/modal/" + image_name + '.txt')
-            amodal_label_filepath = os.path.join(intermediate_dir, "labels/" + split + "/orig/amodal/" + image_name + '.txt')
+            
+            modal_label_dir = os.path.join(intermediate_dir, "labels/" + split + "/orig/modal/")
+            if not os.path.exists(modal_label_dir):
+                os.makedirs(modal_label_dir)
+            modal_label_filepath = modal_label_dir + image_name + '.txt'
+            
+            amodal_label_dir = os.path.join(intermediate_dir, "labels/" + split + "/orig/amodal/")
+            if not os.path.exists(amodal_label_dir):
+                os.makedirs(amodal_label_dir)
+            amodal_label_filepath = amodal_label_dir + image_name + '.txt'
             write_label_to_file(modal_yolo_label, modal_label_filepath)
             write_label_to_file(amodal_yolo_label, amodal_label_filepath)
 
@@ -207,33 +222,35 @@ def copy_orig(orig_data_dir, intermediate_dir):
 
 
 def copy_aug(aug_data_dir_path, intermediate_dir, orig_data_dir):
-    splits = os.listdir(aug_data_dir_path)
-    if '.DS_Store' in splits:
-        splits.remove('.DS_Store')
-
     img_count = 0
     print(f"[{datetime.now()}] Starting to copy augmented images")
     modal_amodal_different_counts = {}  # { "train": List[int, int], "val": List[int, int], "test": List[int, int] }
 
-    for split in splits:
+    for split in os.listdir(aug_data_dir_path):
+        if split.find('.DS_Store') != -1:
+            continue
+
         print(f"[{datetime.now()}] Starting {split}")
         split_path = aug_data_dir_path + split + '/'
-        occlusion_classes = os.listdir(split_path)
-        if '.DS_Store' in occlusion_classes:
-            occlusion_classes.remove('.DS_Store')
-
         modal_amodal_different_count = [0,0]
+
+        occlusion_classes = os.listdir(split_path)
         for occlusion_class in occlusion_classes:
+            if occlusion_class.find(".DS_Store") != -1:
+                continue
+
             occlusion_class_path = split_path + occlusion_class + '/'
             images_dir_path = occlusion_class_path + 'images/'
             labels_dir_path = occlusion_class_path + 'labels/'
 
             image_filenames = os.listdir(images_dir_path)
-            if '.DS_Store' in image_filenames:
-                image_filenames.remove('.DS_Store')
-            
             for image_filename in image_filenames:
+                if image_filename.find(".DS_Store") != -1:
+                    continue
+
+                ##############
                 # Copy image
+                ##############
                 s_img = os.path.join(images_dir_path, image_filename)
                 d_img_dir = os.path.join(intermediate_dir, "images/" + split + '/aug/' + occlusion_class + '/')
                 if not os.path.exists(d_img_dir):
@@ -241,14 +258,17 @@ def copy_aug(aug_data_dir_path, intermediate_dir, orig_data_dir):
                 d_img = os.path.join(d_img_dir, image_filename)
                 shutil.copy2(s_img, d_img)
 
+                ##############
                 # Copy label
+                ##############
+                # Get occlusion yolo path
                 image_name, _ = get_filename_and_extension(image_filename)
                 image_id, occlusion_id = tuple(image_name.split('.'))
                 occlusion_yolo_label_path = os.path.join(labels_dir_path, image_id + '.annotated.' + occlusion_id + '.txt')
 
                 # Get occlusion and original kitti labels
-                # original_yolo_label_filepath = orig_data_dir + 'yolo_labels/' + split + '/' + image_id + '.txt'
-                original_kitti_label_filepath = orig_data_dir + 'kitti_labels/' + split + '/' + image_id + '.txt'
+                split_type = get_split_type(split)
+                original_kitti_label_filepath = orig_data_dir + 'kitti_labels/' + split_type + '/' + image_id + '.txt'
                 original_amodal_kitti_label = read_background_annotation_kitti(original_kitti_label_filepath)
 
                 occlusion_yolo_label = read_background_annotation_yolo(occlusion_yolo_label_path)
@@ -284,7 +304,7 @@ def copy_aug(aug_data_dir_path, intermediate_dir, orig_data_dir):
                 if img_count % 100 == 0:
                     print(f"[{datetime.now()}] Finished {img_count} images")
 
-        modal_amodal_different_counts[split] = modal_amodal_different_count
+        modal_amodal_different_counts[split_type] = modal_amodal_different_count
     
     print(f"[{datetime.now()}] Augmented modal-amodal-different Count: ", modal_amodal_different_counts)
     print(f"[{datetime.now()}] Finished augmented images")
@@ -309,12 +329,11 @@ if __name__ == "__main__":
     if not os.path.exists(inter_path):
         os.makedirs(inter_path)
     
-    # # Copy original images into output dir
-    copy_orig(orig_path, inter_path)
+    # Copy original images into output dir
+    copy_orig(orig_path, inter_path, aug_path)
 
     # Copy augmented images into output dir
-    make_aug_dirs(inter_path)     # Because we aren't using the aug for the baseline, but future scripts expect this structure
-    # copy_aug(aug_path, inter_path, orig_path)
+    copy_aug(aug_path, inter_path, orig_path)
 
 """
 Example Usage:
